1472a63bcd496b20f4720dec545312f9b9d6de77
usmancynosure <usmanwaris0987@gmail.com>
2025-02-19 13:02:23 -0800

    AI Enginner weel 1 day 1(basic python)

---
+++ basic.py
@@ -0,0 +1,19 @@
+# basic operation
+# integer 
+age=2.5
+height=5.9
+
+# String 
+name="usman waris"
+
+
+# List 
+names=["usman", "Ali", "Waleed"]
+print(names[0])
+
+# Tuples
+Regno=(104, 23, 11)
+
+# Dictionary
+College={"Ali":"DPS", "USMAN ":"COMSATS ", "Age":22}
+print(College["Ali"])
