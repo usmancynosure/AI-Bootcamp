62aed81fa90a6ac185c122fcfffade70c860f17d
usmancynosure <usmanwaris0987@gmail.com>
2025-03-04 23:47:28 -0800

    Broadcasting | Aggregation | Random seed

---
+++ Data Science Essential/day2_week2_broadcasting.py
@@ -0,0 +1,42 @@
+# Broadcasting -->To perform arithemtic operation on arrays on different shape
+import numpy as np
+
+# make the array from the list
+array=np.array([1,2,3])
+# add the 10 -->by applying broadcasting technique automaticlly
+# print(array+10) #This will add the 10 to each element in the array 
+
+
+# create the 2 by 3 matrix
+matrix=np.array([[2,3,4], [6,7,8]])
+vector=np.array([1,0,1])  #this means 3  rows and i coloum
+# print(matrix.shape)
+# print(vector.shape)
+# print(matrix+vector)
+
+
+# Aggregation function-->compute summary statistic for array
+# useful in AI-->calculate mean, median ,sum , standard deviation
+
+# create the 2 by 3 matrix
+matrix=np.array([[2,3,4], [6,7,8]])
+# perform the aggregation methods
+print(f"sum of elements are : {np.sum(matrix)}")
+print(f"Mean of matrix is :{np.mean(matrix)}")
+print(f"Standart Deviation is {np.std(matrix)}")
+print(f"Sum across rows {np.sum(matrix ,axis=1)}")
+print(f"Sum across col {np.sum(matrix ,axis=0)}")
+
+
+# Booloean Indexing and Filtering  -->specially usefull when traning models and value chahneg in data sets
+arr=np.array([1,2,3,4])
+
+even=arr[arr%2==0]
+print("evens, ",even)
+
+arr[arr>2]=0
+print("Modified array: ", arr)
+
+
+
+
