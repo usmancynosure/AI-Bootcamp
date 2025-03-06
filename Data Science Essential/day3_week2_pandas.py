4df39d609d6e634617cfda0d320c7dd194697fc0
usmancynosure <usmanwaris0987@gmail.com>
2025-03-05 23:34:10 -0800

    Pandas Basics

---
+++ Data Science Essential/day3_week2_pandas.py
@@ -0,0 +1,7 @@
+# pandas -->python powerful library for data manipulation and analysis 
+# Pandas Data structure  -->series and dataframe
+# series -->one dimentional labeled  array cappable of holding data of any type 
+# data frame -->two dimentional labeled data structure like a table 
+
+import pandas as pd
+
